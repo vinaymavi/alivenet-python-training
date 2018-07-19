bd9ebf5a9bdc262d6e9b7f960f22fa24e8d9a35c
mohitalivenetsolutions <mohitalivenetsolutions@gmail.com>
2018-07-19 18:27:15 +0530

    input name  and pass in function then print the name else none

---
+++ m4.py
@@ -0,0 +1,7 @@
+def getName(msg):
+        if msg!='' :
+            print("You are ",msg)
+        else:
+            print("Your are None")
+myMessage=input("Enter your name: ")
+getName(myMessage)
